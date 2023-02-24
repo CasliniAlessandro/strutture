66169b55a3055d95605987b284e64462c3033b71
caslini <caslini.alessandro.studente@itispaleocapa.it>
2023-02-24 13:31:14 +0100

    Aggiungere i file di progetto.

---
+++ strutture/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace strutture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nome = new System.Windows.Forms.TextBox();
+            this.prezzo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.salva = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cancella = new System.Windows.Forms.Button();
+            this.modifica = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // nome
+            // 
+            this.nome.Location = new System.Drawing.Point(133, 99);
+            this.nome.Name = "nome";
+            this.nome.Size = new System.Drawing.Size(100, 20);
+            this.nome.TabIndex = 0;
+            // 
+            // prezzo
+            // 
+            this.prezzo.Location = new System.Drawing.Point(315, 99);
+            this.prezzo.Name = "prezzo";
+            this.prezzo.Size = new System.Drawing.Size(100, 20);
+            this.prezzo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(312, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Prezzo";
+            // 
+            // salva
+            // 
+            this.salva.Location = new System.Drawing.Point(35, 99);
+            this.salva.Name = "salva";
+            this.salva.Size = new System.Drawing.Size(75, 23);
+            this.salva.TabIndex = 4;
+            this.salva.Text = "salva";
+            this.salva.UseVisualStyleBackColor = true;
+            this.salva.Click += new System.EventHandler(this.salva_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(516, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(252, 362);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(133, 155);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // cancella
+            // 
+            this.cancella.Location = new System.Drawing.Point(35, 152);
+            this.cancella.Name = "cancella";
+            this.cancella.Size = new System.Drawing.Size(75, 23);
+            this.cancella.TabIndex = 7;
+            this.cancella.Text = "cancella";
+            this.cancella.UseVisualStyleBackColor = true;
+            this.cancella.Click += new System.EventHandler(this.cancella_Click);
+            // 
+            // modifica
+            // 
+            this.modifica.Location = new System.Drawing.Point(35, 200);
+            this.modifica.Name = "modifica";
+            this.modifica.Size = new System.Drawing.Size(75, 23);
+            this.modifica.TabIndex = 8;
+            this.modifica.Text = "modifica";
+            this.modifica.UseVisualStyleBackColor = true;
+            this.modifica.Click += new System.EventHandler(this.modifica_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(133, 203);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.modifica);
+            this.Controls.Add(this.cancella);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.salva);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.prezzo);
+            this.Controls.Add(this.nome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nome;
+        private System.Windows.Forms.TextBox prezzo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button salva;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button cancella;
+        private System.Windows.Forms.Button modifica;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ strutture/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace strutture
+{
+    public partial class Form1 : Form
+    {   public struct prodotto
+        {
+            public string nome;
+            public float prezzo;
+        }
+        public prodotto [] p;
+        public int dim;
+        public Form1()
+        {
+            InitializeComponent();
+            p=new prodotto[100];
+            dim = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salva_Click(object sender, EventArgs e)
+        {
+            p[dim].nome = nome.Text;
+            p[dim].prezzo = float.Parse(prezzo.Text);
+            dim++;
+            visualizza(p);
+            
+        }
+        public string prodString(prodotto p)
+        {
+            return "Nome:"+p.nome+" prezzo:"+p.prezzo.ToString();
+        }
+        public void visualizza(prodotto[] pp)
+        {
+            listView1.Items.Clear();
+            for (int i = 0; i < dim; i++)
+            {
+                listView1.Items.Add(prodString(p[i]));
+            }
+        }
+
+
+        public bool Cancella(prodotto[] p, string nom)
+        {
+            bool ritorno=false;
+            for (int i = 0; i < dim; i++)
+            {
+                if (p[i].nome == nom)
+                {
+                    int var = i;
+                    while (var < dim - 1)
+                    {
+                        p[var] = p[var + 1];
+                        var++;
+                    }
+                    dim--;
+                    ritorno = true;
+                }
+            }
+            return ritorno;
+
+
+        }
+
+        private void cancella_Click(object sender, EventArgs e)
+        {
+            if (Cancella(p, textBox1.Text))
+
+            {
+                visualizza(p);
+                MessageBox.Show("Elemento cancellato");
+            }
+            else
+            {
+                MessageBox.Show("Elemento non trovato");
+            }
+
+
+
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void modifica_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+
+
